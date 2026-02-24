0610c03d04c65eed95e1ca35031be328c4b72641
vika <37137412+hi-im-vika@users.noreply.github.com>
2026-02-23 21:18:04 -0800

    Update sample creds file to include firebase api key

--- keys.sample.py
+++ keys.sample.py
@@ -1,2 +1,3 @@
 mapbox_access_token = ''    # put your mapbox access token here
 translink_api_key = ''      # put your translink api key here
+firebase_apikey = ''        # put your firebase web api key here
