af7584996805cd980efbe8d44e539a425babb5f9
vika <37137412+hi-im-vika@users.noreply.github.com>
2026-03-05 16:02:25 -0800

    Create index for lookup performance

--- src/index.py
+++ src/index.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, render_template, request, flash, session, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, Index
+from sqlalchemy.exc import OperationalError
 from google.transit import gtfs_realtime_pb2
 import time
 import requests
@@ -61,6 +62,24 @@ else:
 
 Models = {}
 
+def create_gtfs_indexes(engine):
+    indexes = [
+        # index for route lookup from stop code
+        Index("idx_stop_times_stop_id", Models["stop_times"].__table__.c.stop_id),
+        Index("idx_stop_times_trip_id", Models["stop_times"].__table__.c.trip_id),
+        Index("idx_trips_route_id",     Models["trips"].__table__.c.route_id),
+
+        # index for shape lookup from trip id
+        Index("idx_trips_shape_id",  Models["trips"].__table__.c.shape_id),
+        Index("idx_shapes_shape_id", Models["shapes"].__table__.c.shape_id),
+    ]
+
+    for idx in indexes:
+        try:
+            idx.create(engine)
+            print(f"Indexed: {idx.name}")
+        except OperationalError:
+            print(f"Skipped: {idx.name} (exists)")
 
 with app.app_context():
     db.reflect()
@@ -75,6 +94,8 @@ with app.app_context():
         )
         Models[table_name] = model
         print(f"Model reflected: {table_name}")
+    create_gtfs_indexes(db.engine)
+
 # with sqlite3.connect(db_path) as con:
 #     cur = con.cursor()
 #     cur.execute('SELECT trip_id FROM stop_times WHERE stop_id=11088')
