3d0e26cde38fc0c824e1f6277064ecac5faa2ba8
JCslam14 <91910105+JCslam14@users.noreply.github.com>
2026-04-10 09:42:39 -0700

    created docker-compose.yml, modified config to have .env inclusion

--- src/config.py
+++ src/config.py
@@ -6,9 +6,13 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "dev-secret-key")
-    GTFS_VEHICLE_URL = f'https://gtfsapi.translink.ca/v3/gtfsposition?apikey={keys.translink_api_key}'
-    GTFS_TRIP_URL = f"https://gtfsapi.translink.ca/v3/gtfsrealtime?apikey={keys.translink_api_key}"
-    WEB_API_KEY = keys.firebase_apikey
-    MAPBOX_ACCESS_TOKEN = keys.mapbox_access_token
+    MAPBOX_ACCESS_TOKEN = os.environ.get("MAPBOX_ACCESS_TOKEN")
+    TRANSLINK_API_KEY = os.environ.get("TRANSLINK_API_KEY")
+    FIREBASE_APIKEY = os.environ.get("FIREBASE_APIKEY")
+    GTFS_VEHICLE_URL = f'https://gtfsapi.translink.ca/v3/gtfsposition?apikey={TRANSLINK_API_KEY}'
+    GTFS_TRIP_URL = f"https://gtfsapi.translink.ca/v3/gtfsrealtime?apikey={TRANSLINK_API_KEY}"
+    #WEB_API_KEY = keys.firebase_apikey
+    #MAPBOX_ACCESS_TOKEN = keys.mapbox_access_token
+
     SERVICE_ACCOUNT_PATH = os.getenv("FIREBASE_SERVICE_ACCOUNT", "serviceAccountKey.json")
-    FIREBASE_LOGIN = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={WEB_API_KEY}"
+    FIREBASE_LOGIN = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={FIREBASE_APIKEY}"
