e334743d878f0c5ef3cc5d844deb7c021413300e
JCslam14 <91910105+JCslam14@users.noreply.github.com>
2026-04-13 20:23:22 -0700

    added logger to app

--- src/app.py
+++ src/app.py
@@ -8,10 +8,13 @@ from blueprints.dashboard import dashboard_bp
 from blueprints.api import api_bp
 from blueprints.data_geojson import data_geojson_bp
 from utils.logging_config import logging_setup
+import logging
 
 app = Flask(__name__)
 app.config.from_object(Config)
 logging_setup()
+logger = logging.getLogger(__name__)
+
 # Register blueprints
 app.register_blueprint(dashboard_bp)  # Handles root route /
 app.register_blueprint(auth_bp)
@@ -20,6 +23,8 @@ app.register_blueprint(api_bp)
 app.register_blueprint(data_geojson_bp)
 
 if __name__ == "__main__":
+    logger.info("Starting app")
     app.run(host="0.0.0.0", port=5000, debug=True)
+
     #app.run(debug=True, port=5000)
 
