81c0f262a3d8e148bd731946c8b67b3da6d91b6e
vika <37137412+hi-im-vika@users.noreply.github.com>
2026-04-14 09:35:06 -0700

    Move db code from index.py to models.py

--- src/index.py
+++ src/index.py
@@ -1,7 +1,4 @@
 from flask import Flask, jsonify, render_template, request, flash, session, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, Index
-from sqlalchemy.exc import OperationalError
 from google.transit import gtfs_realtime_pb2
 import time
 import requests
@@ -24,7 +21,6 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("FLASK_SECRET_KEY", "dev-secret-key")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///static.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
 # Replace this with your agency's GTFS-Realtime vehicle positions URL
 GTFS_VEHICLE_URL = f'https://gtfsapi.translink.ca/v3/gtfsposition?apikey={keys.translink_api_key}'
@@ -40,61 +36,6 @@ try:
 except:
     print("types.pkl not found, fetching")
     fetch_types()
-
-# get GTFS static data if not already existing
-db_path = os.path.join(app.instance_path, "static.db")
-
-if not os.path.isfile(db_path):
-    print("Static data not found, fetching")
-    fetch_gtfs_static(app.instance_path)
-    with app.app_context():
-        db.create_all()     # create database if not yet existing
-        for f in os.listdir(app.instance_path):
-            if f.endswith('.txt'):
-                name = f.split('.')[0]
-                path = os.path.join(app.instance_path, f)
-                df = pandas.read_csv(path)
-                df.to_sql(name, db.engine, if_exists="replace", index=False)
-                os.remove(path)
-                print(f'{f} imported into db')
-else:
-    print("Static data found")
-
-Models = {}
-
-def create_gtfs_indexes(engine):
-    indexes = [
-        # index for route lookup from stop code
-        Index("idx_stops_stop_code",    Models["stops"].__table__.c.stop_code),
-        Index("idx_stop_times_stop_id", Models["stop_times"].__table__.c.stop_id),
-        Index("idx_stop_times_trip_id", Models["stop_times"].__table__.c.trip_id),
-
-        # index for shape lookup from trip id
-        Index("idx_trips_shape_id",  Models["trips"].__table__.c.shape_id),
-        Index("idx_shapes_shape_id", Models["shapes"].__table__.c.shape_id),
-    ]
-
-    for idx in indexes:
-        try:
-            idx.create(engine)
-            print(f"Indexed: {idx.name}")
-        except OperationalError:
-            print(f"Skipped: {idx.name} (exists)")
-
-with app.app_context():
-    db.reflect()
-    for table_name, table in db.metadata.tables.items():
-        pk_col = Column("rowid", Integer, primary_key=True)
-        table.append_column(pk_col)
-
-        model = type(
-            table_name.capitalize(),
-            (db.Model,),
-            {"__table__": table}
-        )
-        Models[table_name] = model
-        print(f"Model reflected: {table_name}")
-    create_gtfs_indexes(db.engine)
 
 # def load_stopcode_to_stopid(stops_path: str) -> dict[str, str]:
 #     m = {}
---
+++ src/models.py
@@ -0,0 +1,57 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer
+
+# # get GTFS static data if not already existing
+# db_path = os.path.join(app.instance_path, "static.db")
+
+# if not os.path.isfile(db_path):
+#     print("Static data not found, fetching")
+#     fetch_gtfs_static(app.instance_path)
+#     with app.app_context():
+#         db.create_all()     # create database if not yet existing
+#         for f in os.listdir(app.instance_path):
+#             if f.endswith('.txt'):
+#                 name = f.split('.')[0]
+#                 path = os.path.join(app.instance_path, f)
+#                 df = pandas.read_csv(path)
+#                 df.to_sql(name, db.engine, if_exists="replace", index=False)
+#                 os.remove(path)
+#                 print(f'{f} imported into db')
+# else:
+#     print("Static data found")
+
+# Models = {}
+
+# def create_gtfs_indexes(engine):
+#     indexes = [
+#         # index for route lookup from stop code
+#         Index("idx_stops_stop_code",    Models["stops"].__table__.c.stop_code),
+#         Index("idx_stop_times_stop_id", Models["stop_times"].__table__.c.stop_id),
+#         Index("idx_stop_times_trip_id", Models["stop_times"].__table__.c.trip_id),
+
+#         # index for shape lookup from trip id
+#         Index("idx_trips_shape_id",  Models["trips"].__table__.c.shape_id),
+#         Index("idx_shapes_shape_id", Models["shapes"].__table__.c.shape_id),
+#     ]
+
+#     for idx in indexes:
+#         try:
+#             idx.create(engine)
+#             print(f"Indexed: {idx.name}")
+#         except OperationalError:
+#             print(f"Skipped: {idx.name} (exists)")
+
+# with app.app_context():
+#     db.reflect()
+#     for table_name, table in db.metadata.tables.items():
+#         pk_col = Column("rowid", Integer, primary_key=True)
+#         table.append_column(pk_col)
+
+#         model = type(
+#             table_name.capitalize(),
+#             (db.Model,),
+#             {"__table__": table}
+#         )
+#         Models[table_name] = model
+#         print(f"Model reflected: {table_name}")
+#     create_gtfs_indexes(db.engine)
